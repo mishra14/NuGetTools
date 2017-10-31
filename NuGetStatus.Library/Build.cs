33aabd6a71eac88db8527df7f01a95008d64b979
Ankit Mishra <anmishr@microsoft.com>
2017-10-31 15:02:14 -0700

    starting work on nugetstatus

---
+++ NuGetStatus.Library/Build.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuGetStatus.Library
+{
+    public class Build
+    {
+        public Links Links { get; set; }
+        public int Id { get; set; }
+        public int BuildNumber { get; set; }
+        public string Status { get; set; }
+        public string Result { get; set; }
+        public string SourceCommit { get; set; }
+        public string SourceBranch { get; set; }
+    }
+
+    public class Links
+    {
+        public string Self { get; set; }
+        public string Build { get; set; }
+        public string Commit { get; set; }
+        public string Timeline { get; set; }
+    }
+}
---
+++ NuGetStatus.Library/VSTSUtil.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NuGetStatus.Library
+{
+    public class VSTSUtil
+    {
+        // build definition api - https://devdiv.visualstudio.com/DefaultCollection/DevDiv/_apis/build/builds?definitions=5868&statusFilter=completed&$top=1&api-version=2.0
+        // release definition api - 
+
+    }
+}
